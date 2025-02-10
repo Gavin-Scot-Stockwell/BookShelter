15f93f3f03479577f6ed3ba605121161c16c0ef8
agnemi <a.j.gnemi@gmail.com>
2025-02-10 13:44:54 -0700

    Test files removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom"
 
-import Navbar2 from "./components/Navbar2"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <Navbar2 />
+      <Navbar/>
         <main>
           <Outlet />
         </main>
